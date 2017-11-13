75928edeb6ae0123ee36e9bc0b30bb1574aa6173
iron-cherep <jacobxblue@gmail.com>
2017-11-13 23:38:22 +0300

    убрал лишние файлы, перевёл комментарии в gulpfile, добавил выбор proxy/dev-сервер

--- gulpfile.js
+++ gulpfile.js
@@ -20,64 +20,65 @@ var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 var uglify       = require('gulp-uglify');
 
-// See https://github.com/austinpray/asset-builder
+// Подробнее: https://github.com/austinpray/asset-builder
 var manifest = require('asset-builder')('./assets/manifest.json');
 
-// `path` - Paths to base asset directories. With trailing slashes.
-// - `path.source` - Path to the source files. Default: `assets/`
-// - `path.dist` - Path to the build directory. Default: `dist/`
+// `path` - Пути к статике
+// - `path.source` - Путь к исходникам. По умолчанию: `assets/`
+// - `path.dist` - Путь к собранной статике. По умолчанию: `dist/`
 var path = manifest.paths;
 
-// `config` - Store arbitrary configuration values here.
+// `config` - Здесь можно сохранить дополнительные настройки для использования в gulpfile
 var config = manifest.config || {};
 
-// `globs` - These ultimately end up in their respective `gulp.src`.
-// - `globs.js` - Array of asset-builder JS dependency objects. Example:
-//   ```
-//   {type: 'js', name: 'main.js', globs: []}
-//   ```
-// - `globs.css` - Array of asset-builder CSS dependency objects. Example:
-//   ```
-//   {type: 'css', name: 'main.css', globs: []}
-//   ```
-// - `globs.fonts` - Array of font path globs.
-// - `globs.images` - Array of image path globs.
-// - `globs.bower` - Array of all the main Bower files.
 var globs = manifest.globs;
-
-// `project` - paths to first-party assets.
-// - `project.js` - Array of first-party JS assets.
-// - `project.css` - Array of first-party CSS assets.
 var project = manifest.getProjectGlobs();
 
-// CLI options
+// Опции для командной строки
 var enabled = {
-  // Enable static asset revisioning when `--production`
+  // Добавить хэши к статике для борьбы с кэшированием старых версий `--production`
   rev: argv.production,
-  // Disable source maps when `--production`
+  // Отключить source-maps `--production`
   maps: !argv.production,
-  // Fail styles task on error when `--production`
+  // Прервать процесс сборки при ошибке сборки стилей `--production`
   failStyleTask: argv.production,
-  // Fail due to JSHint warnings only when `--production`
+  // Прервать процесс сборки при получении предупреждений JSHint `--production`
   failJSHint: argv.production,
-  // Strip debug statments from javascript when `--production`
+  // Убрать debug-опции из js `--production`
   stripJSDebug: argv.production
 };
 
-// Path to the compiled assets manifest in the dist directory
+// Опции для BrowserSync
+if ( config.useProxy ) {
+  // стандартные настройки Sage для работы с фронтендом поверх работающего сервера
+  config.BSOptions = {
+    files: ['{lib,templates}/**/*.php', '*.php'],
+    proxy: config.devUrl
+  };
+} else {
+  // использовать для разработки dev-сервер BrowserSync
+  config.BSOptions = {
+    server: ".",
+    ghostMode: false,
+    open: true,
+    cors: true
+  };
+}
+
+// Путь к манифесту собранной статики (для получения актуальных имён файлов с хэшами)
 var revManifest = path.dist + 'assets.json';
 
-// Error checking; produce an error rather than crashing.
+// Обработка ошибок - выдавать лог ошибки вместо падений процесса сборки
 var onError = function(err) {
   console.log(err.toString());
   this.emit('end');
 };
 
-// ## Reusable Pipelines
-// See https://github.com/OverZealous/lazypipe
+// ## Повторяемые пайплайны/процессы
+// Подробнее: https://github.com/OverZealous/lazypipe
 
-// ### CSS processing pipeline
-// Example
+// ### Процесс обработки CSS
+// Пример:
 // ```
 // gulp.src(cssFiles)
 //   .pipe(cssTasks('main.css')
@@ -96,14 +97,14 @@ var cssTasks = function(filename) {
     })
     .pipe(function() {
       return gulpif('*.scss', sass({
-        outputStyle: 'nested', // libsass doesn't support expanded yet
+        outputStyle: 'nested', // libsass не поддерживает expanded-версии
         precision: 10,
         includePaths: ['.'],
         errLogToConsole: !enabled.failStyleTask
       }));
     })
     .pipe(concat, filename)
-    .pipe(autoprefixer, {
+    .pipe(autoprefixer, { // браузеры для автопрефиксера
       browsers: [
         'last 2 versions',
         'android 4',
@@ -123,8 +124,8 @@ var cssTasks = function(filename) {
     })();
 };
 
-// ### JS processing pipeline
-// Example
+// ### Процесс обработки JS
+// Пример:
 // ```
 // gulp.src(jsFiles)
 //   .pipe(jsTasks('main.js')
@@ -151,9 +152,8 @@ var jsTasks = function(filename) {
     })();
 };
 
-// ### Write to rev manifest
-// If there are any revved files then write them to the rev manifest.
-// See https://github.com/sindresorhus/gulp-rev
+// ### Процесс записи ревизий статики с хэшами в манифест
+// Подробнее: https://github.com/sindresorhus/gulp-rev
 var writeToManifest = function(directory) {
   return lazypipe()
     .pipe(gulp.dest, path.dist + directory)
@@ -165,13 +165,11 @@ var writeToManifest = function(directory) {
     .pipe(gulp.dest, path.dist)();
 };
 
-// ## Gulp tasks
+// ## Задачи для Gulp
 // Run `gulp -T` for a task summary
 
-// ### Styles
-// `gulp styles` - Compiles, combines, and optimizes Bower CSS and project CSS.
-// By default this task will only log a warning if a precompiler error is
-// raised. If the `--production` flag is set: this task will fail outright.
+// ### Стили
+// `gulp styles` - Компилирует, объединяет и оптимизирует CSS проекта и зависимостей Bower
 gulp.task('styles', ['wiredep'], function() {
   var merged = merge();
   manifest.forEachDependency('css', function(dep) {
@@ -190,9 +188,8 @@ gulp.task('styles', ['wiredep'], function() {
     .pipe(writeToManifest('styles'));
 });
 
-// ### Scripts
-// `gulp scripts` - Runs JSHint then compiles, combines, and optimizes Bower JS
-// and project JS.
+// ### JS
+// `gulp scripts` - Компилирует, объединяет и оптимизирует JS проекта и зависимостей Bower
 gulp.task('scripts', ['jshint'], function() {
   var merged = merge();
   manifest.forEachDependency('js', function(dep) {
@@ -206,9 +203,9 @@ gulp.task('scripts', ['jshint'], function() {
     .pipe(writeToManifest('scripts'));
 });
 
-// ### Fonts
-// `gulp fonts` - Grabs all the fonts and outputs them in a flattened directory
-// structure. See: https://github.com/armed/gulp-flatten
+// ### Шрифты
+// `gulp fonts` - Берёт все шрифты и помещает плоской структурой (без вложенности папок) в папку со статикой
+// Подробнее: https://github.com/armed/gulp-flatten
 gulp.task('fonts', function() {
   return gulp.src(globs.fonts)
     .pipe(flatten())
@@ -216,8 +213,8 @@ gulp.task('fonts', function() {
     .pipe(browserSync.stream());
 });
 
-// ### Images
-// `gulp images` - Run lossless compression on all the images.
+// ### Изображения
+// `gulp images` - Запускает оптимизацию изображений без потерь
 gulp.task('images', function() {
   return gulp.src(globs.images)
     .pipe(imagemin([
@@ -233,7 +230,7 @@ gulp.task('images', function() {
 });
 
 // ### JSHint
-// `gulp jshint` - Lints configuration JSON and project JS.
+// `gulp jshint` - Линтинг JS и JSON файлов
 gulp.task('jshint', function() {
   return gulp.src([
     'bower.json', 'gulpfile.js'
@@ -243,44 +240,36 @@ gulp.task('jshint', function() {
     .pipe(gulpif(enabled.failJSHint, jshint.reporter('fail')));
 });
 
-// ### Clean
-// `gulp clean` - Deletes the build folder entirely.
+// ### Чистка
+// `gulp clean` - Удаляет папку со статикой перед сборкой новых файлов
 gulp.task('clean', require('del').bind(null, [path.dist]));
 
-// ### Watch
-// `gulp watch` - Use BrowserSync to proxy your dev server and synchronize code
-// changes across devices. Specify the hostname of your dev server at
-// `manifest.config.devUrl`. When a modification is made to an asset, run the
-// build step for that asset and inject the changes into the page.
-// See: http://www.browsersync.io
+// ### Watch-процессы
+// `gulp watch` - запускает BrowserSync и следит за изменением файлов,
+// после чего запускает соответствующие процессы сборки.
+// Подробнее: http://www.browsersync.io
 gulp.task('watch', function() {
-  browserSync.init({
-      server: ".",
-      ghostMode: false,
-      notify: false,
-      open: true,
-      cors: true,
-      ui: false
-  });
+  browserSync.init(config.BSOptions);
   gulp.watch([path.source + 'styles/**/*'], ['styles']);
   gulp.watch([path.source + 'scripts/**/*'], ['jshint', 'scripts']);
   gulp.watch([path.source + 'fonts/**/*'], ['fonts']);
   gulp.watch([path.source + 'images/**/*'], ['images']);
   gulp.watch(['bower.json', 'assets/manifest.json'], ['build']);
+  gulp.watch(['*.html','*.php']).on('change', browserSync.reload);
 });
 
 // ### Build
-// `gulp build` - Run all the build tasks but don't clean up beforehand.
-// Generally you should be running `gulp` instead of `gulp build`.
+// `gulp build` - Запускает все процессы сборки, используется общим
+// процессом Gulp после очистки папки со статикой
 gulp.task('build', function(callback) {
   runSequence('styles',
-              'scripts',
-              ['fonts', 'images'],
-              callback);
+    'scripts',
+    ['fonts', 'images'],
+    callback);
 });
 
 // ### Wiredep
-// `gulp wiredep` - Automatically inject Less and Sass Bower dependencies. See
+// `gulp wiredep` - Автоматически добавляет зависимости Bower в CSS и JS
 // https://github.com/taptapship/wiredep
 gulp.task('wiredep', function() {
   var wiredep = require('wiredep').stream;
@@ -293,7 +282,7 @@ gulp.task('wiredep', function() {
 });
 
 // ### Gulp
-// `gulp` - Run a complete build. To compile for production run `gulp --production`.
+// `gulp` - Запустить процесс сборки. Для production-версии без sourcemap и хэшей: gulp --production
 gulp.task('default', ['clean'], function() {
   gulp.start('build');
 });
